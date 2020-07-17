f381015bf8f0e033a2de5024c0e7520d136d44ab
azizulhakim0794 <67514785+azizulhakim0794@users.noreply.github.com>
2020-07-17 23:57:21 +0600

    finish work

--- assignment.js
+++ assignment.js
@@ -1,3 +1,53 @@
-var b = 5;
-var a = 3;
-console.log(a + b);
+      //feetToMile
+ 
+ function feetToMile (feet) {
+
+     var mile = feet/5280;
+     return mile;
+ }
+ var result = feetToMile(3453);
+ console.log(result);
+
+         //  woodCalculator!!
+
+ function woodCalculator(chair, table, bed) {
+     var chairCount = chair * 1; 
+     var tableCount = table * 3; 
+     var bedCount = bed * 5; 
+
+     var totalWood = chairCount + tableCount + bedCount;
+
+     return totalWood;
+ }
+
+ var result = woodCalculator(4, 5, 6);
+ console.log(result);
+
+
+      //brickcalculator
+
+function brickcalculator() {
+   
+   var firstTop = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+   var secondTop = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+   var thirdTop = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+   
+}
+
+
+
+
+      //tinyFrind
+
+ function tinyFrind(word) {
+    var max = word[0].length;
+    word.map(mini => max = Math.max(max, mini.length));
+    result = word.filter(mini => mini.length == max);
+    return result;
+  }
+
+  var result = tinyFrind(['Akash', 'Abdulallah', 'Masum','Nur','Shahadat'])
+ 
+  console.log(result)
+ 
+
